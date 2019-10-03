e8b5289425f071de46bf9ba8adf7bc1f423b08bd
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-03 15:12:57 -0400

    user urls and views

---
+++ bucketListApp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import include, path                    
+from rest_framework import routers
+from . import views #if you include the name of the file after ".", you don't need to include "views." in router.register
+
+router = routers.DefaultRouter()
+# r is short for regular expression (regex)
+# similar to express's app.use("/issueitem/", someRouter)
+# trailing slashes are not necessary in django unless it's being called via AJAX
+router.register(r'user', views.UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)), #api (or whatever) can be added as prefix for all routes
+]
--- bucketListApp/views.py
+++ bucketListApp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets    
+from .serializers import UserSerializer     
+from .models import User  
+from datetime import datetime  
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):  
+    # below is a multiline string  
+    """    
+    API endpoint that allows users to be viewed or edited.    
+    """    
+    queryset = User.objects.all()    
+    serializer_class = UserSerializer

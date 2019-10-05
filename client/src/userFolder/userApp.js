f7964b2037d35d49c0a08814806722a7b43d60a2
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-05 14:24:47 -0400

    users is still undefined, new directories and files created

--- client/src/userFolder/userApp.js
+++ client/src/userFolder/userApp.js
@@ -1,13 +1,5 @@
 import React from "react"
 
-// display list of user's email and issues
-// const userIssueList = (user) => (
-//     <div>
-//         {user.email}
-//         {/* {issueList(user.issues)} */}
-//     </div>
-// )
-
 // this shows 1 user as option for select tag 
 const userPreview = (user) => (
     <option value={user.id}>{user.userName}</option>
@@ -33,12 +25,16 @@ class NewUserForm extends React.Component {
         newUser[event.target.name] = event.target.value;
         this.setState(newUser)
     }
+    
+    handleInput = (event) => {
+        let newUser = { ...this.state };
+        newUser[event.target.name] = event.target.value;
+        this.setState(newUser)
+    }
 
     handleSubmit = (event) => {
         event.preventDefault();
-        // this is just to test to see if the correct data is being called
-        // console.log("iss: ", this.state)
-
+        // change this.state of userName and email
         this.props.addNewUser(this.state)
         this.setState({ userName: "", email: "" })
     }
@@ -56,16 +52,16 @@ class NewUserForm extends React.Component {
 const testUsers =
 {
     1:
-    {       
+    {
         id: 1,
-        email: "Shawn@SoftwareEngineer.com",
         userName: "Shawn",
+        email: "Shawn@SoftwareEngineer.com",
     },
     7:
     {
         id: 7,
-        email: "Asha@UXDesigner.com",
         userName: "Asha",
+        email: "Asha@UXDesigner.com",
         // issues: [
         //     { description: "a Joe test issue", id: 1, createdOn: "2019-09-27T15:05:18.180058Z" },
         //     { description: "a Joe test issue 2", id: 2, createdOn: "2019-09-28T15:05:18.180058Z" },
@@ -75,59 +71,52 @@ const testUsers =
 }
 
 const getUsersFromServer = () =>
-  fetch("/api/user/")
-    .then(res => res.json())
+    fetch("/api/user/")
+        .then(res => res.json())
+            .then((users) => objectFromListById(users))
 
 const saveUserToServer = (newUser) =>
-  fetch('/api/user/',
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(newUser)
-    }
-  ).then(res => {
-      console.log('saver user to server response', res)
-      res.json()})
-  .catch((error) => {
-      console.log(error)
-  })
+    fetch('/api/user/',
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newUser)
+        }
+    ).then(res => {
+        console.log('saver user to server response', res)
+        res.json()
+    })
+        .catch((error) => {
+            console.log(error)
+        })
+
+const objectFromListById = (users) =>
+    //convert from an array of user objects to an
+    //object of user objects where the keys are user ids
+    users.reduce((obj, user) => {
+        //get all issues belonging to the user
+        // user.issues = issues.filter(issue => issue.user === user.id);
+        obj[user.id] = user;
+        return obj;
+    }, {})
 
 class UserApp extends React.Component {
 
     state = {
         currentUser: 1,
-        users: testUsers
+        // users: testUsers
+        users: []
     }
 
-    // componentDidMount = () => {
-    //     getUsersandIssuesFromServer()
-    //         .then(users => {
-    //             this.setState({ users })
-    //         })
-    // }
-
-    // getNextIssueId = () =>
-    //     Math.max(...this.getCurrentUser().issues.map(issue => issue.id)) + 1
+    componentDidMount = () => {
+        getUsersFromServer()
+            .then(users => {
+                this.setState({ users })
+            })
+    }
 
     getNextUserId = () =>
-        // {
-        // Math.max(...this.getAllUsers().map(user => user.id)) === Infinity ? 1 :
         Math.max(...this.getAllUsers().map(user => user.id)) + 1
-    // console.log(Math.max(...this.getAllUsers().map(user => user.id)))
-    // }
-
-
-    // // this function below is just to show that the correct information is being called upon submission 
-    // addNewIssueCurrentUser = (description) => {
-    //     saveIssueToServer({ description, status: true, user: this.state.currentUser })
-    //         .then(newIssue => {
-    //             let users = { ...this.state.users }
-
-    //             users[this.state.currentUser].issues.push(newIssue);
-
-    //             this.setState({ users });
-    //         })
-    // }
 
     addNewUser = (newUserInfo) => {
         // console.log('newuserinfo', newUserInfo)
@@ -156,12 +145,6 @@ class UserApp extends React.Component {
         Object.values(this.state.users)
 
 
-    // getAllIssues = () =>
-    //     // .flatMap takes array of array and makes one massive array
-    //     // this function is to eliminate {recentIssues(testUsers[0].issues)}
-    //     this.getAllUsers().flatMap(user => user.issues)
-    // // this.getAllIssues().map(user => user.issues).flat //this is a longer way of doing the .flatMap function above
-
     setCurrentUser = (currentUser) => {
         this.setState({ currentUser })
     }
@@ -170,18 +153,13 @@ class UserApp extends React.Component {
         <div className='container'>
             <aside className='sidebar'>
                 <NewUserForm addNewUser={this.addNewUser} />
-                {/* <NewIssueForm addNewIssue={this.addNewIssueCurrentUser} />
-                {recentIssues(this.getAllIssues())} */}
             </aside>
             <article className='mainContent'>
-                
-                            <div>
-                                <h1>Users</h1>
-                                {userList(this.getAllUsers(), this.state.currentUser, this.setCurrentUser)}
-                                {/* <h1>User's Issues</h1> */}
-                                {/* {userIssueList(this.getCurrentUser())} */}
-                            </div>
-                        
+
+                <div>
+                    <h1>Users</h1>
+                    {userList(this.getAllUsers(), this.state.currentUser, this.setCurrentUser)}
+                </div>
             </article>
         </div>
     )

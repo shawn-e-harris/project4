3d758316f5e5e9cbfaee9c3d7de5580ac142a323
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-07 12:10:12 -0400

    Unhandled Rejection (TypeError): Cannot read property 'id' of undefined

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import UserFunctions from "./userFolder/UserFunctions"
+// import UserFunctions from "./userFolder/UserFunctions"
 // import BucketListItemFunctions from "./bucketListFolder/BucketListItemFunctions"
 // import CheckListItemFunctions from "./checkListFolder/CheckListItemFunctions"
+import UserApp from "./userFolder/UserApp"
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        <UserFunctions/>
-        {/* <BucketListItemFunctions/>
+        <UserApp/>
+        {/* <UserFunctions/>
+        <BucketListItemFunctions/>
         <CheckListItemFunctions/> */}
       </div>
     )
--- client/src/userFolder/userApp.js
+++ client/src/userFolder/userApp.js
@@ -1,156 +1,82 @@
-// import React from "react"
-// import Axios from "axios"
-// import NewUserForm from "./NewUserForm"
+import React from "react"
+import NewUserForm from "./NewUserForm"
+import {getUsersFromServer} from "./UserAPICalls"
+import {userList} from "../importsFolder/functions"
+import {saveUserToServer} from "../userFolder/UserAPICalls"
 
-// // shows userName as option for select tag and stores id as value
-// const userPreview = (user) => (
-//     <option value={user.id}>{user.userName}</option>
-// )
+// test data for user
+const testUsers = [
+{ id: 1, userName: "Shawn", email: "Shawn@SoftwareEngineer.com" },
+{ id: 2, userName: "Asha", email: "Asha@UXDesigner.com" },
+]
 
-// // changes value of UserId to match User selected
-// const userList = (users, currentUserId, onChange) => (
-//     <select value={currentUserId} onChange={(event) => onChange(event.target.value)}>
-//         {users.map(userPreview)}
-//     </select>
-// )
+class UserApp extends React.Component {
 
-// // changed to add userNumber to match state in the App Component
-// const testUsers =
-// {
-//     1:
-//     {
-//         id: 1,
-//         userName: "Shawn",
-//         email: "Shawn@SoftwareEngineer.com",
-//     },
-//     7:
-//     {
-//         id: 7,
-//         userName: "Asha",
-//         email: "Asha@UXDesigner.com",
-//     }
-// }
-
-// // const getUsersFromServer = () =>
-// //     fetch("/api/user/")
-// //         .then(res => res.json())
-// //             .then((users) => objectFromListById(users))
-
-// const getUsersFromServer = () => {
-//     Axios.get("/api/user/") //get prefix
-//         .then(res => res.json())// //create promise
-//             // this.setState({ activities: results.data.allActivities })
-//             // console.log(res.data)
-//             // console.table(results.data.allActivities)
-//         //})
-//         .catch(error => {
-//             console.log(error)
-//         })
-// }
-
-// // const saveUserToServer = (newUser) =>
-// //     fetch('/api/user/',
-// //         {
-// //             method: "POST",
-// //             headers: { "Content-Type": "application/json" },
-// //             body: JSON.stringify(newUser)
-// //         }
-// //     ).then(res => {
-// //         console.log('save user to server response', res)
-// //         res.json()
-// //     })
-// //         .catch((error) => {
-// //             console.log(error)
-// //         })
-
-// Axios.post("/api/user", userName)
-//             .then(results => {
-//                 this.setState({ userName: results.data.userName })
-//                 // console.log('save user to server response', res)
-//             })
-//             .catch(error => {
-//                 console.log(error)
-//             })
-    
+    state = {
+        currentUser: 1,
+        users: testUsers
+    }
 
 
+    componentDidMount = () => {
+        getUsersFromServer()
+            .then(users => {
+                this.setState({ users })
+            })
+    }
 
-// const objectFromListById = (users) =>
-//     //convert from an array of user objects to an
-//     //object of user objects where the keys are user ids
-//     users.reduce((obj, user) => {
-//         //get all issues belonging to the user
-//         // user.issues = issues.filter(issue => issue.user === user.id);
-//         obj[user.id] = user;
-//         return obj;
-//     }, {})
+    // componentDidMount() {
+    //     this.getUsersFromServer();
+    // }
 
-// class UserApp extends React.Component {
+    // getNextUserId = () =>
+    //     Math.max(...this.getAllUsers().map(user => user.id)) + 1
 
-//     state = {
-//         currentUser: 1,
-//         users: testUsers
-//         // users: []
-//     }
+    addNewUser = (newUserInfo) => {
+        // console.log('newuserinfo', newUserInfo)
+        return saveUserToServer(newUserInfo)
+            .then(newUser => {
+                console.log(newUser)
+                // newUser.issues = [];
+                //newUser.id = this.getNextUserId();
 
-//     // componentDidMount = () => {
-//     //     getUsersFromServer()
-//     //         .then(users => {
-//     //             this.setState({ users })
-//     //         })
-//     // }
+                let users = { ...this.state.users }
+                // console.log(users)
 
-//     componentDidMount() {
-//         this.getUsersFromServer();
-//     }
+                users[newUser.id] = newUser;
 
-//     // getNextUserId = () =>
-//     //     Math.max(...this.getAllUsers().map(user => user.id)) + 1
+                this.setState({ users, currentUser: newUser.id });
+            })
+    }
 
-//     addNewUser = (newUserInfo) => {
-//         // console.log('newuserinfo', newUserInfo)
-//         this.saveUserToServer(newUserInfo)
-//             .then(newUser => {
-//                 console.log(newUser)
-//                 // newUser.issues = [];
-//                 //newUser.id = this.getNextUserId();
-
-//                 let users = { ...this.state.users }
-
-//                 users[newUser.id] = newUser;
-
-//                 this.setState({ users, currentUser: newUser.id });
-//             })
-//     }
-
-//     getCurrentUser = () =>
-//         // will also need to change .map in 
-//         this.state.users[this.state.currentUser]
+    getCurrentUser = () =>
+        // will also need to change .map in 
+        this.state.users[this.state.currentUser]
 
 
-//     getAllUsers = () =>
-//         Object.values(this.state.users)
+    getAllUsers = () =>
+        Object.values(this.state.users)
 
 
-//     setCurrentUser = (currentUser) => {
-//         this.setState({ currentUser })
-//     }
+    setCurrentUser = (currentUser) => {
+        this.setState({ currentUser })
+    }
 
-//     render = () => (
-//         <div className='container'>
-//             <aside className='sidebar'>
-//                 <NewUserForm addNewUser={this.addNewUser} />
-//             </aside>
-//             <article className='mainContent'>
+    render = () => (
+        <div className='container'>
+            <aside className='sidebar'>
+                <NewUserForm addNewUser={this.addNewUser} />
+            </aside>
+            <article className='mainContent'>
 
-//                 <div>
-//                     <h1>Users</h1>
-//                     {userList(this.getAllUsers(), this.state.currentUser, this.setCurrentUser)}
-//                 </div>
-//             </article>
-//         </div>
-//     )
-// }
+                <div>
+                    <h1>Users</h1>
+                    {userList(this.getAllUsers(), this.state.currentUser, this.setCurrentUser)}
+                </div>
+            </article>
+        </div>
+    )
+}
 // //  ONLY USE COLON (:id) TO SET SPECIFIC ROUTE
 
-// export default UserApp;
+export default UserApp;

6b11a35a0af8b35bc97ddb56998e0f3dfa6fa77e
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-07 00:35:25 -0400

    test user working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import UserApp from "./userFolder/UserApp"
+import UserFunctions from "./userFolder/UserFunctions"
+// import UserApp from "./userFolder/UserApp"
 // import BucketListItemApp from "./bucketListFolder/BucketListApp"
 // import CheckListItemApp from "./checkListFolder/CheckListApp"
 
@@ -8,9 +9,10 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <UserApp />
-        {/* <BucketListItemApp />
-        <CheckListItemApp /> */}
+        <UserFunctions/>
+        {/* <UserApp /> */}
+        {/* <BucketListItemApp /> */}
+        {/*<CheckListItemApp /> */}
       </div>
     )
   }

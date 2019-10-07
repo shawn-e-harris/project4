0295cc587f74632611e0f1dabbaa7765018886cd
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-07 10:47:29 -0400

    user display working

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import UserFunctions from "./userFolder/UserFunctions"
-import BucketListItemFunctions from "./bucketListFolder/BucketListItemFunctions"
-import CheckListItemFunctions from "./checkListFolder/CheckListItemFunctions"
+// import BucketListItemFunctions from "./bucketListFolder/BucketListItemFunctions"
+// import CheckListItemFunctions from "./checkListFolder/CheckListItemFunctions"
 
 export default class App extends React.Component {
   render() {

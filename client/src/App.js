d5316394d0d96c7026bfd5cc494dac82befeb224
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-07 01:04:15 -0400

    array of test bLI working

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './App.css';
 import UserFunctions from "./userFolder/UserFunctions"
-// import UserApp from "./userFolder/UserApp"
-// import BucketListItemApp from "./bucketListFolder/BucketListApp"
-// import CheckListItemApp from "./checkListFolder/CheckListApp"
+import BucketListItemFunctions from "./bucketListFolder/BucketListItemFunctions"
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <UserFunctions/>
-        {/* <UserApp /> */}
-        {/* <BucketListItemApp /> */}
-        {/*<CheckListItemApp /> */}
+        <BucketListItemFunctions/>
       </div>
     )
   }

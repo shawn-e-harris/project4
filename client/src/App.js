22d0d80cb7dbf042cc5d33be8610a8ac29a13496
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-07 00:12:39 -0400

    comment out bLI and cLI

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 import UserApp from "./userFolder/UserApp"
-import BucketListItemApp from "./bucketListFolder/BucketListApp"
-import CheckListItemApp from "./checkListFolder/CheckListApp"
+// import BucketListItemApp from "./bucketListFolder/BucketListApp"
+// import CheckListItemApp from "./checkListFolder/CheckListApp"
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <UserApp />
-        <BucketListItemApp />
-        <CheckListItemApp />
+        {/* <BucketListItemApp />
+        <CheckListItemApp /> */}
       </div>
     )
   }
--- client/src/bucketListFolder/newBucketListItemForm.js
+++ client/src/bucketListFolder/newBucketListItemForm.js
@@ -1,31 +1,31 @@
-import React from "react"
+// import React from "react"
 
-// this is the class component that will set the state of the BucketListItem component  
-export default class NewBucketListItemForm extends React.Component {
-    state = {
-        bucketListItemName: "",
-        email: ""
-    }
+// // this is the class component that will set the state of the BucketListItem component  
+// export default class NewBucketListItemForm extends React.Component {
+//     state = {
+//         bucketListItemName: "",
+//         email: ""
+//     }
 
-    handleInput = (event) => {
-        let newBucketListItem = { ...this.state };
-        newBucketListItem[event.target.name] = event.target.value;
-        this.setState(newBucketListItem)
-    }
+//     handleInput = (event) => {
+//         let newBucketListItem = { ...this.state };
+//         newBucketListItem[event.target.name] = event.target.value;
+//         this.setState(newBucketListItem)
+//     }
     
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        // change this.state of bucketListItemName and email
-        this.props.addNewBucketListItem(this.state)
-        this.setState({ bucketListItemName: "", email: "" })
-    }
+//     handleSubmit = (event) => {
+//         event.preventDefault();
+//         // change this.state of bucketListItemName and email
+//         this.props.addNewBucketListItem(this.state)
+//         this.setState({ bucketListItemName: "", email: "" })
+//     }
 
-    render = () => (
-        <form onSubmit={this.handleSubmit}>
-            <input type="text" name="bucketListItemName" value={this.state.bucketListItemName} onChange={this.handleInput} placeholder="BucketListItem Name" />
-            <input type="email" name="email" value={this.state.email} onChange={this.handleInput} placeholder="Email" />
-            <input type="submit" value="New BucketListItem" />
-        </form>
-    )
-}
+//     render = () => (
+//         <form onSubmit={this.handleSubmit}>
+//             <input type="text" name="bucketListItemName" value={this.state.bucketListItemName} onChange={this.handleInput} placeholder="BucketListItem Name" />
+//             <input type="email" name="email" value={this.state.email} onChange={this.handleInput} placeholder="Email" />
+//             <input type="submit" value="New BucketListItem" />
+//         </form>
+//     )
+// }

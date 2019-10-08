f1f94415d585f1ab04a1a4e89a6ee79fe93a6a52
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-08 10:12:53 -0400

    Unhandled Rejection (TypeError): this.props.getUsersFromServer is not a function

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 // import UserFunctions from "./userFolder/UserFunctions"
 // import BucketListItemFunctions from "./bucketListFolder/BucketListItemFunctions"
 // import CheckListItemFunctions from "./checkListFolder/CheckListItemFunctions"
@@ -10,8 +11,17 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <UserApp/>
-        <BucketListItemApp/>
+        <Router>
+            <Link to ="/api/">  Home  </Link> 
+            <Link to ="/api/user/" >  Users  </Link> 
+            <Link to ="/api/bucketlistitem/" >  Bucket List Items  </Link> 
+          <Switch>
+          <Route exact path="/api/user" component={UserApp} />
+          <Route exact path="/api/bucketlistitem" component={BucketListItemApp} />
+          </Switch>
+        </Router>
+
+        
         {/* <UserFunctions/> */}
         {/* <BucketListItemFunctions/>
         <CheckListItemFunctions/> */}

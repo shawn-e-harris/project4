5b42e428dbff4af6f610a389fc9e95533a4b7657
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-11 17:29:16 -0400

    navbar displaying on userpage

--- bucketListApp/models.py
+++ bucketListApp/models.py
@@ -1,16 +1,20 @@
 from django.db import models
 from datetime import datetime
 
+default_u = "https://unsplash.com/photos/SKICLmVW_eg"
+default_bl = "https://images.unsplash.com/photo-1568323993144-20d546ba585d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+
 class User(models.Model):
     # this is like schema in express IssueItemSchema = mongoose.Schema({description : String, done : Boolean})
     userName = models.CharField(max_length=10)
     email = models.EmailField(max_length=50)
-    picture = models.CharField(max_length=5000)
+    picture = models.CharField(max_length=5000, default=default_u)
 
 class BucketListItem(models.Model):
     # this is like schema in express IssueItemSchema = mongoose.Schema({description : String, done : Boolean})
     bucketListName = models.CharField(max_length=80)
     status = models.BooleanField()
+    picture = models.CharField(max_length=5000, default=default_bl)
     userId = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users") #User refers to class User(must be above to read properly, casing does not matter, related name must be plural)
 
 class CheckListItem(models.Model):
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import UApp from "./userFolder/UApp"
-import HomePage from "./userFolder/UApp"
-import SingleU from './userFolder/SingleU';
-import SingleBL from './bucketListFolder/SingleBL';
+import UApp from "./components/userFolder/UApp"
+import HomePage from "./components/CSS/HomePage"
+import SingleU from './components/userFolder/SingleU';
+import SingleBL from './components/bucketListFolder/SingleBL';
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
         <Router>
-            <Link to ="/">  Home  </Link> 
-            <Link to ="/user/" >  Users  </Link> 
+            {/* <Link to ="/">  Home  </Link> 
+            <Link to ="/user/" >  Users  </Link>  */}
           <Switch>
+          <Route exact path="/user/" component={HomePage} />
           {/* <Route exact path="/user/" component={UApp} /> */}
-          <Route exact path="/user/" component={UApp} />
           <Route exact path="/user/:userId" component={SingleU} />
           <Route exact path="/bucketlistitem/:bucketId" component={SingleBL} />
           </Switch>

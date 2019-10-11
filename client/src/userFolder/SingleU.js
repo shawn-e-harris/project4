696792fdf9e57852a1d6cd520550b32d34a03b9f
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-11 10:23:23 -0400

    delete user working

--- client/src/userFolder/SingleU.js
+++ client/src/userFolder/SingleU.js
@@ -2,11 +2,12 @@ import React from 'react'
 import Axios from 'axios'
 import BLForm from "../bucketListFolder/BLForm"
 import BLApp from '../bucketListFolder/BLApp'
-// import UDelete from '../userFolder/UDelete'
+import {Redirect} from 'react-router-dom'
 
 export default class SingleU extends React.Component {
     state = {
-        user: {}
+        user: {},
+        redirectToHome: false
     }
 
     getSingleUser = () =>
@@ -19,16 +20,33 @@ export default class SingleU extends React.Component {
         this.getSingleUser()
     }
 
+    handleDeleteShow = () => {
+        Axios.delete(`/api/user/${this.props.match.params.userId}/`)
+        .then(() => {
+            // this is the trigger to change setState for redirectToHome
+            this.setState({redirectToHome: true})
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
     render() {
+        // conditional rendering for redirectToHome is truthy
+        if(this.state.redirectToHome) {
+            return(
+                <Redirect to="/user" />
+            )
+        }
         return (
             <div>
                 <BLForm {...this.props}
                     userId={this.state.user.id}
                 />
                 <p>{this.state.user.id} <br />{this.state.user.userName} <br /> {this.state.user.email}</p> <img src={this.state.user.picture}/>
+                <button onClick={this.handleDeleteShow}>Delete</button>
                 <BLApp {...this.props}
                 />
-                {/* <UDelete/> */}
             </div>
         )
     }

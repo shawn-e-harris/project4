b247719cfb47e240242ffbf8b2c2338116cd9586
Shawn Harris <shawn.harris3510@gmail.com>
2019-10-11 12:33:04 -0400

    beginning styling

--- client/src/bucketListFolder/SingleBL.js
+++ client/src/bucketListFolder/SingleBL.js
@@ -2,21 +2,19 @@ import React from 'react'
 import Axios from 'axios'
 import CLForm from "../checkListFolder/CLForm"
 import CLApp from "../checkListFolder/CLApp"
-import {Redirect} from 'react-router-dom'
+// import {Redirect} from 'react-router-dom'
 
 export default class SingleBL extends React.Component {
     state = {
         bucketListItem: {},
-        redirectToBL: false
+        // redirectToBL: false
     }
 
-    getSingleBucketListItem = () => {
+    getSingleBucketListItem = () =>
     Axios.get(`/api/bucketlistitem/${this.props.match.params.bucketId}/`)
     .then(res => {
-        console.log(res)
         this.setState({bucketListItem: res.data})
     })
-}
 
     getRelatedBucketListItems = () =>{
 
@@ -34,31 +32,31 @@ export default class SingleBL extends React.Component {
         this.getRelatedBucketListItems();
     }
 
-    handleDeleteBL = () => {
-        Axios.delete(`/api/bucketlistitem/${this.props.match.params.bucketId}/`)
-        .then(() => {
-            // this is the trigger to change setState for redirectToHome
-            this.setState({redirectToBL: true})
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    }
+    // handleDeleteBL = () => {
+    //     Axios.delete(`/api/bucketlistitem/${this.props.match.params.bucketId}/`)
+    //     .then(() => {
+    //         // this is the trigger to change setState for redirectToHome
+    //         this.setState({redirectToBL: true})
+    //     })
+    //     .catch(err => {
+    //         console.log(err);
+    //     })
+    // }
     
     render() {
-        // conditional rendering for redirectToHome is truthy
-        if(this.state.redirectToBL) {
-            return(
-                this.getRelatedBucketListItems
-            )
-        }
+        // // conditional rendering for redirectToHome is truthy
+        // if(this.state.redirectToBL) {
+        //     return(
+        //         <Redirect to={`/user/${this.state.match.params.userId}`} />
+        //     )
+        // }
         return (
             <div>
                 <CLForm {...this.props}
                 bucketId= {this.state.bucketListItem.id}
                 />
                 <p>{this.state.bucketListItem.id} <br/>{this.state.bucketListItem.bucketListName} <br/> {this.state.bucketListItem.status} </p> 
-                <button onClick={this.handleDeleteBL}>Delete</button>
+                {/* <button onClick={this.handleDeleteBL}>Delete</button> */}
                 <CLApp {...this.props}
                 />
             </div>
